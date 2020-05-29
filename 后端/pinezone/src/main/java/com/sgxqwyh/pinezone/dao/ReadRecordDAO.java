5bad18e0af96dd1f25127fa2df931fd8b1a2438e
bob402 <60547704+bob402@users.noreply.github.com>
2020-05-29 20:48:55 +0800

    Update ReadRecordDAO.java

--- 后端/pinezone/src/main/java/com/sgxqwyh/pinezone/dao/ReadRecordDAO.java
+++ 后端/pinezone/src/main/java/com/sgxqwyh/pinezone/dao/ReadRecordDAO.java
@@ -4,6 +4,10 @@ import com.sgxqwyh.pinezone.pojo.ReadRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReadRecordDAO extends JpaRepository<ReadRecordEntity, Long> {
+    //根据用户id返回用户历史阅读记录
+    public List<ReadRecordEntity> findByUid(int Uid);
 }
