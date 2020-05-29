91b533ab873627cde7e1615e283999fdb1a23f84
hujh4779 <823362797@qq.com>
2020-05-29 16:04:16 +0800

    Create StatisticsController（新增总阅读量统计接口）
    新增总阅读量统计接口，返回最早日期至今每天的总阅读量

---
+++ 后端/pinezone/src/main/java/com/sgxqwyh/pinezone/controller/StatisticsController.java
@@ -0,0 +1,84 @@
+package com.sgxqwyh.pinezone.controller;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.sgxqwyh.pinezone.dao.CommentDAO;
+import com.sgxqwyh.pinezone.dao.LikeDAO;
+import com.sgxqwyh.pinezone.dao.ReadRecordDAO;
+import com.sgxqwyh.pinezone.pojo.ReadRecordEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/v1")
+public class StatisticsController {
+
+    @Autowired
+    private ReadRecordDAO readRecordDAO;
+    @Autowired
+    private LikeDAO likeDAO;
+    @Autowired
+    private CommentDAO commentDAO;
+
+    //锦浩
+    @PostMapping(value = "/article/read")
+    public ReadRecordEntity addRecord(@RequestParam("uid") Integer uid, @RequestParam("aid") Integer aid) {
+        ReadRecordEntity readRecordEntity = new ReadRecordEntity();
+        readRecordEntity.setUid(uid);
+        readRecordEntity.setAid(aid);
+        readRecordEntity.setState(1);
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        readRecordEntity.setDate(timestamp);
+        return readRecordDAO.save(readRecordEntity);
+    }
+
+    @GetMapping(value = "/statistics/articleReadNum")
+    public JSONArray getArticleReadNum() throws ParseException {
+        List<ReadRecordEntity> readRecordEntityList = readRecordDAO.findAll();
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        String start = readRecordEntityList.get(0).getDay();
+        for (ReadRecordEntity readRecordEntity : readRecordEntityList) {
+            String date = readRecordEntity.getDay();
+            if (hashMap.containsKey(date)) {
+                hashMap.put(date, hashMap.get(date) + 1);
+            } else {
+                hashMap.put(date, 1);
+            }
+        }
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = dateFormat.parse(start);
+        Date today = new Date();
+        JSONArray jsonArray = new JSONArray();
+        while (true) {
+            JSONObject jsonObject = new JSONObject();
+            if (hashMap.containsKey(start)) {
+                jsonObject.put("date", start);
+                jsonObject.put("num", hashMap.get(start));
+                jsonArray.add(jsonObject);
+            } else {
+                jsonObject.put("date", start);
+                jsonObject.put("num", 0);
+                jsonArray.add(jsonObject);
+            }
+            String todayString = dateFormat.format(today);
+            if (todayString.equals(start)) {
+                break;
+            } else {
+                Calendar calendar = new GregorianCalendar();
+                calendar.setTime(startDate);
+                calendar.add(calendar.DATE,1); //把日期往后增加一天,整数往后推,负数往前移动
+                startDate = calendar.getTime(); //这个时间就是日期往后推一天的结果
+                start = dateFormat.format(startDate);
+                //System.out.println(start);
+            }
+        }
+        return jsonArray;
+    }
+}
