dbce045ba41a43e8f023237ffb86bd993c319835
bob402 <60547704+bob402@users.noreply.github.com>
2020-05-29 20:49:08 +0800

    Update ReadRecordEntity.java

--- 后端/pinezone/src/main/java/com/sgxqwyh/pinezone/pojo/ReadRecordEntity.java
+++ 后端/pinezone/src/main/java/com/sgxqwyh/pinezone/pojo/ReadRecordEntity.java
@@ -2,16 +2,20 @@ package com.sgxqwyh.pinezone.pojo;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 @Entity
 @Table(name = "read_record", schema = "", catalog = "pinezone")
-public class ReadRecordEntity {
+public class ReadRecordEntity implements Comparable<ReadRecordEntity>{
     private long id;
     private int uid;
     private long aid;
     private Timestamp date;
     private int state;
+    private String day;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -64,6 +68,17 @@ public class ReadRecordEntity {
         this.state = state;
     }
 
+    @Transient
+    public String getDay() {
+        DateFormat df= new SimpleDateFormat("yyyy-MM-dd");
+        return df.format(new Date(this.date.getTime()));
+    }
+
+    public void setDay() {
+        DateFormat df= new SimpleDateFormat("yyyy-MM-dd");
+        this.day = df.format(new Date(this.date.getTime()));
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -79,6 +94,27 @@ public class ReadRecordEntity {
     @Override
     public int hashCode() {
         return Objects.hash(id, uid, aid, date, state);
+    }
+
+    @Override
+    public int compareTo(ReadRecordEntity r) {
+        int i ;
+        if(this.date.getTime()>r.date.getTime()) {
+            i = 1;
+        }
+        else if(this.date.getTime()<r.date.getTime()) {
+            i = -1;
+        }
+        else {
+            i = 0;
+        }
+        if(i == 0) {
+            if (r.id > this.id)
+                i = 1;
+            else
+                i = -1;
+        }
+        return -i;
     }
 }
 
